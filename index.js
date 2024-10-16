b50f7b6085d75ada6e8058878809afabe8cb23fb
Tharun Teja <pellakurutharunteja@gmail.com>
2024-10-16 17:49:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const display = document.getElementById('display');
+
+const appendToDisplay = (input) => {
+    display.value += input;
+
+}
+
+const clearDisplay = () => {
+    display.value = "";
+
+}
+
+const calculate = () => {
+    display.value = eval(display.value);
+
+}
